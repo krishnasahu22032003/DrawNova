8474e6cbbbfccbb9d5626ae0420cb2db4bbc4ae8
krishna sahu <krishna.sahu.work@gmail.com>
2026-05-20 16:02:59 +0530

    Start server function created and working

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,42 +1,13 @@
-import express from "express";
-import { prisma } from "@repo/db/client";
+import express from "express" ; 
+import StartServer from "./lib/startServer";
 
-const app = express();
+const app = express() ; 
 
 app.use(express.json());
 
-app.post("/signin", async (req, res) => {
-  try {
-    const { email, password ,username } = req.body;
 
-    // dummy user creation for testing
-    const user = await prisma.user.create({
-      data: {
-        email,
-        password,
-        username
-      },
-    });
 
-    return res.status(200).json({
-      success: true,
-      message: "User created successfully",
-      user,
-    });
-  } catch (error) {
-    console.error(error);
 
-    return res.status(500).json({
-      success: false,
-      message: "Something went wrong",
-    });
-  }
-});
 
-app.get("/", (req, res) => {
-  res.send("HTTP Backend Running 🚀");
-});
 
-app.listen(3002, () => {
-  console.log("Server running on port 3002");
-});
+StartServer(app);
--- apps/http-backend/src/lib/startServer.ts
+++ apps/http-backend/src/lib/startServer.ts
@@ -1,9 +1,16 @@
 import { Express } from "express";
+import ENV_SECRETS from "./ENV";
 
 export default function StartServer(app: Express) {
 
-try{
-    
-}
+    const http_port = ENV_SECRETS.PORT;
 
-}
+    try {
+        app.listen(http_port, (() => {
+            console.log(`App is running on port ${http_port}`);
+        }))
+    } catch (err) {
+        console.error((err as Error).message);
+        process.exit(1);
+    }
+} ; 
