703e43beb6c19f863dc1d6ede925682d5ae2d3c8
krishna sahu <krishna.sahu.work@gmail.com>
2026-05-22 18:24:30 +0530

    index.ts file updated in backend

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,14 +1,15 @@
 import express from "express" ; 
 import StartServer from "./lib/startServer";
 import UserRouter from "./routes/UserRoutes";
+import BoardRouter from "./routes/BoardRoutes";
+import cookieParser from "cookie-parser";
 
 const app = express() ; 
 
 app.use(express.json());
+app.use(cookieParser()) ; 
 
-app.use("api/v1/user" , UserRouter)
-
-
-
+app.use("api/v1/user" , UserRouter) ;
+app.use("api/v1/board" , BoardRouter) ;
 
 StartServer(app);
