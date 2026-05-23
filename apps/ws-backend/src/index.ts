7c2bf4d47ae4e084f8002792683e394d5fcea75d
krishna sahu <krishna.sahu.work@gmail.com>
2026-05-23 21:48:30 +0530

    ws server code done

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,1 +1,20 @@
-console.log("hello from ws")
+import { WebSocket, WebSocketServer } from "ws";
+
+const wss = new WebSocketServer({port:8080}) ;
+ 
+const user: WebSocket[] = [] ; 
+
+wss.on("connection", (ws : WebSocket)=>{
+    console.log("websocket connection done") ; 
+
+    ws.on("message" , (message : Buffer)=>{
+        const data = message.toString();
+        console.log(data);
+
+        wss.clients.forEach((client)=>{
+            if(client.readyState === WebSocket.OPEN  && ws !== client){
+                client.send(data)
+            }
+        })
+    })
+})
