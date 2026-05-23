5ce6500e03c50f25d717e950638352570e7d96d0
krishna sahu <krishna.sahu.work@gmail.com>
2026-05-23 23:50:49 +0530

    updated index code ws

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,44 +1,56 @@
 import { WebSocket, WebSocketServer } from "ws";
-import {createServer} from "http" ;
+import { createServer } from "http";
 import { RoomManager } from "./managers/RoomManager";
+import ENV from "./utils/ENV";
 
+const Port = ENV.PORT;
 const server = createServer();
-const wss = new WebSocketServer({server}) ;
-const roomManager = RoomManager.getInstance() ;
+const wss = new WebSocketServer({ server });
+const roomManager = RoomManager.getInstance();
 
-const user: WebSocket[] = [] ; 
+wss.on("connection", (ws: WebSocket) => {
 
-wss.on("connection", (ws : WebSocket)=>{
+    console.log("New Client connected");
 
-    console.log("New Client connected") ; 
+    ws.on("message", (message: Buffer) => {
 
-    ws.on("message" , (message : Buffer)=>{
+        try {
 
-        const data = JSON.parse(message.toString());
+            const data = JSON.parse(message.toString());
 
-        switch(data.type){
+            switch (data.type) {
 
-            case "JOIN_ROOM":
+                case "JOIN_ROOM":
 
-            roomManager.joinRoom(data.payLoad.roomId , ws);
-            break;
+                    roomManager.joinRoom(data.payLoad.roomId, ws);
+                    break;
 
-            case "LEAVE_ROOM":
+                case "LEAVE_ROOM":
 
-            roomManager.leaveRoom(data.payLoad.roomId , ws);
-            break;
+                    roomManager.leaveRoom(data.payLoad.roomId, ws);
+                    break;
 
-            case "DRAW":
+                case "DRAW":
 
-            roomManager.broadcast(data.payLoad.roomId , data , ws) ;
-            break; 
+                    roomManager.broadcast(data.payLoad.roomId, data, ws);
+                    break;
 
+            };
+
+        } catch (err) {
+            console.error("Message parse error:", err)
         };
 
     });
 
-    ws.on("close",()=>{
-        roomManager.removeSocket(ws) ;
+    ws.on("close", () => {
+        roomManager.removeSocket(ws);
+        console.log("Client Disconnected");
     });
+});
+
+server.listen(Port, () => {
+
+    console.log(`Server is running on ${Port}`);
 });
 
