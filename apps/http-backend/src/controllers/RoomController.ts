57c1aaad8da2a7266f9f375a61c8686720cd2cd2
krishna sahu <krishna.sahu.work@gmail.com>
2026-05-23 17:07:48 +0530

    create room code updated

--- apps/http-backend/src/controllers/RoomController.ts
+++ apps/http-backend/src/controllers/RoomController.ts
@@ -1,85 +1,79 @@
-import { Request, Response } from "express";
 import { prisma } from "@repo/db/client";
 import { CreateRoomSchema } from "@repo/validators/Zod";
+import { Request, Response } from "express";
 
-export async function CreateRoom(
-  req: Request,
-  res: Response
-) {
+
+export async function CreateRoom(req: Request, res: Response) {
+
   if (!req.userId) {
     return res.status(401).json({
       success: false,
-      message: "Unauthorized",
+      message: "Invalid User"
     });
-  }
-   
-  const userId = req.userId ; 
+  };
 
-  const parsedData =
-    CreateRoomSchema.safeParse(req.body);
+  const userId = req.userId;
+
+  const parsedData = CreateRoomSchema.safeParse(req.body);
 
   if (!parsedData.success) {
     return res.status(400).json({
       success: false,
-      message: "Invalid input",
-      error: parsedData.error.flatten(),
+      message: "Invalid Input",
+      error: parsedData.error.flatten()
     });
-  }
+  };
 
   const { name, maxUsers } = parsedData.data;
 
   try {
+
     const result = await prisma.$transaction(
+
       async (tx) => {
-        // 1. Create Room
+
         const room = await tx.room.create({
           data: {
-            name,
+            ownerId: userId,
             maxUsers,
-            ownerId: userId
+            name,
           },
         });
 
-        // 2. Create Collaborative Board
         const board = await tx.board.create({
           data: {
             title: `${name} Board`,
-
             type: "ROOM",
-
             roomId: room.id,
-
             elements: [],
-
             appState: {
               zoom: 1,
               scrollX: 0,
               scrollY: 0,
               theme: "dark",
-            },
-          },
+            }
+          }
         });
 
-        // 3. Add Owner As Room Member
-        await tx.roomMembers.create({
+        const roomMembers = await tx.roomMembers.create({
           data: {
             roomId: room.id,
-            userId: userId
-          },
+            userId
+          }
         });
 
         return {
           room,
-          board,
-        };
+          board
+        }
       }
     );
-
     return res.status(201).json({
       success: true,
-      message: "Room created successfully",
-      data: result,
+      message: "Room created Successfully",
+      data: result
     });
+
   } catch (err) {
     console.error(err);
 
@@ -87,5 +81,6 @@ export async function CreateRoom(
       success: false,
       message: "Internal server error",
     });
-  }
-}
+  };
+
+};
