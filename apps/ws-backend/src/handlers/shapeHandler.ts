7fe3dc0064f6ba0af7e8ca18c3f12d5ddfb4cf4f
krishna sahu <krishna.sahu.work@gmail.com>
2026-05-24 23:58:49 +0530

    handleclearBoard function added in shapeHandler

--- apps/ws-backend/src/handlers/shapeHandler.ts
+++ apps/ws-backend/src/handlers/shapeHandler.ts
@@ -1,31 +1,41 @@
 import { RoomManager } from "../managers/RoomManager";
-import { DrawShapePayload , UpdateShapePayload , DeleteShapePayload , CursorMovePayload , ClearBoardPayload } from "../types/message";
+import { DrawShapePayload, UpdateShapePayload, DeleteShapePayload, CursorMovePayload, ClearBoardPayload } from "../types/message";
 import WebSocket from "ws";
 
-const roomManager = RoomManager.getInstance() ; 
+const roomManager = RoomManager.getInstance();
 
-export function handleDrawShape(ws : WebSocket , payLoad : DrawShapePayload){
+export function handleDrawShape(ws: WebSocket, payLoad: DrawShapePayload) {
 
-    roomManager.broadcast(payLoad.roomId , {
-        type : "DRAW",
+    roomManager.broadcast(payLoad.roomId, {
+        type: "DRAW",
         payLoad
-    }, ws) ; 
-
-} ; 
-
-export function handleUpdateShape(ws :WebSocket , payLoad : UpdateShapePayload){
-
-roomManager.broadcast(payLoad.roomId , {
-    type:"UPDATE_SHAPE",
-    payLoad
-},ws) ;
+    }, ws);
 
 };
 
-export function handleDeleteShape(ws:WebSocket  , payLoad : DeleteShapePayload){
+export function handleUpdateShape(ws: WebSocket, payLoad: UpdateShapePayload) {
 
-roomManager.broadcast(payLoad.roomId , {
-    type:"DELETE_SHAPE",
-    payLoad
-},ws); 
+    roomManager.broadcast(payLoad.roomId, {
+        type: "UPDATE_SHAPE",
+        payLoad
+    }, ws);
+
 };
+
+export function handleDeleteShape(ws: WebSocket, payLoad: DeleteShapePayload) {
+
+    roomManager.broadcast(payLoad.roomId, {
+        type: "DELETE_SHAPE",
+        payLoad
+    }, ws);
+};
+
+export function handleClearBoard(
+    ws: WebSocket,
+    payload: ClearBoardPayload
+) {
+    roomManager.broadcast(payload.roomId, {
+        type: "CLEAR_BOARD",
+        payload
+    }, ws);
+}
