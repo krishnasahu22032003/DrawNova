3134cee8a94c245d54ad71aa5b914ff6c5bf48da
krishna sahu <krishna.sahu.work@gmail.com>
2026-05-26 23:07:33 +0530

    toggle theme function created

---
+++ apps/web/components/theme-toogle.tsx
@@ -0,0 +1,27 @@
+import { useTheme } from "next-themes";
+import { Sun, Moon } from "lucide-react";
+
+export function ToggleTheme() {
+
+    const { theme, setTheme } = useTheme();
+
+    return (
+
+        <button onClick={() => {
+            setTheme(theme === "dark" ? "light" : "dark");
+        }} className="">
+
+            {
+                theme === "dark" ? (
+                    <Sun size={18} />
+                ) : (
+                    <Moon size={18} />
+                )
+            }
+
+        </button>
+
+    )
+
+
+};
