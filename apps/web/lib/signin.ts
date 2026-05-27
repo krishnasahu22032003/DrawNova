ae4883a35709143c9efa8e670464d4dc1e0a73b3
krishna sahu <krishna.sahu.work@gmail.com>
2026-05-27 19:22:26 +0530

    signin function done

---
+++ apps/web/lib/signin.ts
@@ -0,0 +1,38 @@
+import AxiosInstance from "./axios";
+import ENV_SECRETS from "./ENV";
+
+interface SignIn {
+
+    email: string,
+    password: string
+
+};
+
+interface SignInResponse {
+
+    success: boolean,
+    message: string,
+    data: {
+        id: string,
+        username: string,
+        email: string
+    }
+
+};
+
+
+export default async function UserSignIn(SignData: SignIn): Promise<SignInResponse> {
+
+    try {
+
+        const res = await AxiosInstance.post<SignInResponse>("/api/v1/user/signin", SignData);
+
+        return res.data;
+
+    } catch (error: any) {
+
+        throw new Error(error.response.data.message || "Signin Failed")
+        
+    };
+
+};
