5a9b513dee88965bbe4474cc86d4f9b214b81ee1
krishna sahu <krishna.sahu.work@gmail.com>
2026-05-27 14:57:02 +0530

    backend cors added frontend env added

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -5,12 +5,17 @@ import BoardRouter from "./routes/BoardRoutes";
 import cookieParser from "cookie-parser";
 import compression from "compression";
 import RoomRouter from "./routes/RoomRoutes";
+import cors from "cors";
 
 const app = express() ; 
 
 app.use(express.json({limit:"10mb"}));
 app.use(cookieParser()) ; 
 app.use(compression()) ; 
+app.use(cors({
+    origin:"http://localhost:3000",
+    credentials:true
+}));
 
 app.use("api/v1/user" , UserRouter) ;
 app.use("api/v1/board" , BoardRouter) ; 
