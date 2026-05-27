c385996e1ccaab7b0f2b67b545029fc42b984cc2
krishna sahu <krishna.sahu.work@gmail.com>
2026-05-27 15:00:29 +0530

    axios instance code done

--- apps/web/lib/axios.ts
+++ apps/web/lib/axios.ts
@@ -1,2 +1,15 @@
 import axios from "axios";
+import ENV_SECRETS from "./ENV";
+
+const AxiosInstance = axios.create({
+
+    baseURL:ENV_SECRETS.BASE_BACKEND_URL ,
+    withCredentials:true
+
+});
+
+export default AxiosInstance ; 
+
+
+
 
