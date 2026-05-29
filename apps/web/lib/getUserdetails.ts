59c4809fedd4665f909a1f8e8f70beec89eb9bda
krishna sahu <krishna.sahu.work@gmail.com>
2026-05-29 11:34:57 +0530

    Get user details function createds

---
+++ apps/web/lib/getUserdetails.ts
@@ -0,0 +1,28 @@
+import AxiosInstance from "./axios";
+
+interface Data {
+
+    id: string,
+    username: string,
+    email: string
+
+};
+
+interface GetUserDetailsResponse {
+
+    success: boolean,
+    data: Data
+};
+
+export default async function GetUserDetails(): Promise<GetUserDetailsResponse> {
+
+    try {
+
+        const res = await AxiosInstance.get<GetUserDetailsResponse>("/api/v1/user/me");
+
+        return res.data;
+    } catch (error: any) {
+        throw new Error(error.Response.data.message || "Something went wrong")
+    };
+
+};
