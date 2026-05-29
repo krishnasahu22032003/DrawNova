93b0466ac90b6bdec6caa68be7cbbf9d00d14cde
krishna sahu <krishna.sahu.work@gmail.com>
2026-05-29 21:42:46 +0530

    shape type added

---
+++ apps/web/types/Shape.ts
@@ -0,0 +1,30 @@
+export type Shape =
+  | RectangleShape
+  | CircleShape
+  | LineShape;
+
+export interface RectangleShape {
+  id: string;
+  type: "rectangle";
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+}
+
+export interface CircleShape {
+  id: string;
+  type: "circle";
+  x: number;
+  y: number;
+  radius: number;
+}
+
+export interface LineShape {
+  id: string;
+  type: "line";
+  startX: number;
+  startY: number;
+  endX: number;
+  endY: number;
+}
