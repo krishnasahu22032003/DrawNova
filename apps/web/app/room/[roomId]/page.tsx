c0ec515534e47cb71b926af34edd29f7bbd9bab9
krishna sahu <krishna.sahu.work@gmail.com>
2026-06-09 21:34:35 +0530

    room header code update

--- apps/web/app/room/[roomId]/page.tsx
+++ apps/web/app/room/[roomId]/page.tsx
@@ -30,7 +30,7 @@ export default function RoomPage() {
 
     return (
         <div className="relative h-screen w-screen overflow-hidden">
-          <RoomHeader/>
+          <RoomHeader roomId={roomId}/>
             <ToolBar selectedTool={selectedTool} setSelectedTool={setSelectedTool} />
 
             <div className="fixed top-4 right-4 z-50 flex items-center gap-2 px-3 py-2 rounded-xl border border-border bg-background/80 backdrop-blur text-xs text-muted-foreground">
